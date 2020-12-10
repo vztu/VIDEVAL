75d33114fb3d53c2ece7a4d3ec2ec6e43232c0c6
Zhengzhong Tu <tzz.fdu@gmail.com>
2020-12-09 23:22:07 -0600

    update demo

--- demo_compute_VIDEVAL_feats.m
+++ demo_compute_VIDEVAL_feats.m
@@ -11,7 +11,17 @@ addpath(genpath('include'));
 % parameters
 algo_name = 'VIDEVAL'; % algorithm name, eg, 'V-BLIINDS'
 data_name = 'TEST_VIDEOS';  % dataset name, eg, 'KONVID_1K'
-data_path = 'videos'; % dataset video path, eg, 'KONVID_1K/KoNViD_1k_videos'
+
+%% *You need to customize here*
+if strcmp(data_name, 'TEST_VIDEOS')
+    data_path = 'videos'; % dataset video path
+elseif strcmp(data_name, 'KONVID_1K')
+    data_path = '/media/ztu/Seagate-ztu-ugc/KONVID_1K/KoNViD_1k_videos';
+elseif strcmp(data_name, 'LIVE_VQC')
+    data_path = '/media/ztu/Seagate-ztu-ugc/LIVE_VQC/VideoDatabase';
+elseif strcmp(data_name, 'YOUTUBE_UGC')
+    data_path = '/media/ztu/Seagate-ztu-ugc/YT_UGC/original_videos';
+end
 
 %%
 % create temp dir to store decoded videos
@@ -28,13 +38,25 @@ feats_mat = zeros(num_videos, 60);
 %% extract features
 % parfor i = 1:num_videos % for parallel speedup
 for i = 1:num_videos
-    try
-        % get video full path
-        video_name = fullfile(data_path,  filelist.video_name{i});
+%     try
+        % get video full path and decoded video name
+        if strcmp(data_name, 'TEST_VIDEOS')
+            video_name = fullfile(data_path,  filelist.video_name{i});
+            yuv_name = fullfile(video_tmp, [filelist.video_name{i}, '.yuv']);
+        elseif strcmp(data_name, 'KONVID_1K')
+            video_name = fullfile(data_path,  [num2str(filelist.flickr_id(i)),'.mp4']);
+            yuv_name = fullfile(video_tmp, [num2str(filelist.flickr_id(i)), '.yuv']);
+        elseif strcmp(data_name, 'LIVE_VQC')
+            video_name = fullfile(data_path, filelist.File{i});
+            yuv_name = fullfile(video_tmp, [filelist.File{i}, '.yuv']);
+        elseif strcmp(data_name, 'YOUTUBE_UGC')
+            video_name = fullfile(data_path, filelist.category{i},...
+                [num2str(filelist.resolution(i)),'P'],[filelist.vid{i},'.mkv']);
+            yuv_name = fullfile(video_tmp, [filelist.vid{i}, '.yuv']);
+        end
         fprintf('\n---\nComputing features for %d-th sequence: %s\n', i, video_name);
 
         % decode video and store in temp dir
-        yuv_name = fullfile(video_tmp, [filelist.video_name{i}, '.yuv']);
         cmd = ['ffmpeg -loglevel error -y -i ', video_name, ' -pix_fmt yuv420p -vsync 0 ', yuv_name];
         system(cmd);  
 
@@ -49,9 +71,9 @@ for i = 1:num_videos
         toc
         % clear cache
         delete(yuv_name)
-    catch
-        feats_mat(i,:) = NaN;
-    end
+%     catch
+%         feats_mat(i,:) = NaN;
+%     end
 end
 % save feature matrix
 save(out_feat_name, 'feats_mat');
